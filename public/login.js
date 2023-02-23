b324a9bbc9b98c70da3754c9021fcbfd9acdf242
Gay <paul6541yau@gmail.com>
2023-02-23 21:00:52 +0800

    Updated login.js, login.css, login.html

--- public/login.js
+++ public/login.js
@@ -26,3 +26,16 @@ function setupLoginPage() {
     })
 }
 setupLoginPage()
+
+let showPassword = false
+document.querySelector(".eye").addEventListener('click', e => {
+    let passwordInput = document.querySelector('#password')
+    if (showPassword) {
+        e.currentTarget.classList = "eye bi bi-eye-slash-fill"
+        passwordInput.type = "text"
+    } else {
+        e.currentTarget.classList = "eye bi bi-eye-fill"
+        passwordInput.type = "password"
+    }
+    showPassword = !showPassword
+})
