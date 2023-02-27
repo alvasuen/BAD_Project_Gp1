9bde9c17b6f2fa7db445b0e3f8dd707009bc241e
Cynthia Lo <cynthiazlo811@gmail.com>
2023-02-27 14:15:17 +0800

    update seed file

---
+++ migrations/20230227033719_update-songs-table.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("songs", (table) => {
+    table.text("image");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("artists", (table) => {
+    table.dropColumn("image");
+  });
+}
