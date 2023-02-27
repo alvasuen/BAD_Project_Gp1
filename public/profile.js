594f14ba68469f1d070adc06589d09fbc30cd05c
Cynthia Lo <cynthiazlo811@gmail.com>
2023-02-27 14:26:15 +0800

    updated the img element src path of the profile.html

--- public/profile.js
+++ public/profile.js
@@ -1,17 +1,17 @@
-// $(".slider").slick({
-//   autoplay: true,
-//   autoplaySpeed: 2000,
-//   speed: 800,
-//   dots: true,
-//   arrows: true,
-//   infinite: true,
-//   focusOnSelect: true,
-//   infinite: true,
-//   speed: 500,
-//   slidesToShow: 4,
-//   slidesToScroll: 1,
-//   touchThreshold: 6,
-// });
+$(".slider").slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 800,
+  dots: true,
+  arrows: true,
+  infinite: true,
+  focusOnSelect: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  touchThreshold: 6,
+});
 
 // $(document).ready(function(){
 //     $('.slider').slick({
