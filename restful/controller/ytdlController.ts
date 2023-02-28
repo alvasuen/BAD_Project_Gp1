973372544e2203da9ac01453ba9ee318a14edd6f
Alva Suen <ymsym98@gmail.com>
2023-02-28 18:32:14 +0800

    modified sanic server

--- restful/controller/ytdlController.ts
+++ restful/controller/ytdlController.ts
@@ -2,25 +2,30 @@ import { Request, Response } from "express";
 import ytdl from "ytdl-core";
 import { createWriteStream } from "fs";
 import { errorHandler } from "../../error";
+import fetch from "cross-fetch";
 
 export class YtdlController {
   constructor() {}
 
-  downloadVideo = async (req: Request, res: Response) => {
+  downloadVideo = (req: Request, res: Response) => {
     try {
-      let URL = req.query.URL;
+      let URL = req.query.url;
       ytdl.getInfo(URL as string).then((data) => {
-        // console.log(data);
-        //get video thumbnail and set it as the cover photo of karaoke song
+        console.log(data);
+
         // console.log(data.videoDetails.thumbnails.at(-1));
+        
+        //download audio only
         ytdl(URL as string, {
           filter: "audioonly",
           quality: "highestaudio",
         }).pipe(
           createWriteStream(
-            `./media_hub/video/${data.videoDetails.videoId}.mp3`
+            `./media_hub/audio/${data.videoDetails.videoId}.mp3`
           )
         );
+
+        //download video only
         ytdl(URL as string, {
           filter: "videoonly",
           quality: "highestvideo",
@@ -29,6 +34,23 @@ export class YtdlController {
             `./media_hub/video/${data.videoDetails.videoId}.mp4`
           )
         );
+        
+
+        // pass video data to sanic server
+        fetch("http://127.0.0.1:8080/sanicytdl", {
+          method: "POST",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify({
+            thumbnail: data.videoDetails.thumbnails.at(-1),
+            ytId: data.videoDetails.videoId,
+            name: data.videoDetails.title,
+          })
+        }).then(()=>{
+          res.status(200).json({success:true});
+        })
+
       });
     } catch (err) {
       console.log(err);
