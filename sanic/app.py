4cfe8ce0db044ae9f3d69354efcc0aee65ca4a92
Alva Suen <ymsym98@gmail.com>
2023-02-28 20:35:51 +0800

    slight modification of app.py

--- sanic/app.py
+++ sanic/app.py
@@ -15,18 +15,18 @@ app = Sanic ("Karaoke")
 def test(request):
    try:
       data = request.json
-      # print(data.data.videoDetails.videoId)
-      with subprocess.Popen(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', '../media_hub/spleeter', '{data.data.videoDetails.videoId}.mp3'], stdout=subprocess.PIPE) as proc:
+      # print(data.ytId)
+      with subprocess.Popen(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', '../media_hub/spleeter', '{data.ytId}.mp3'], stdout=subprocess.PIPE) as proc:
          print(proc.stdout.read())
 
-      old_name1 = r'C:\Users\Alva\Karaoke_Proj\BAD_Project_Gp1\media_hub\spleeter\vocals.wav'
-      new_name2 = r'C:\Users\Alva\Karaoke_Proj\BAD_Project_Gp1\media_hub\spleeter\{data.data.videoDetails.videoId}_vocals.wav'
-      with os.rename(old_name1,new_name2):
+      old_name1 = r'..\media_hub\spleeter\vocals.wav'
+      new_name1 = r'..\media_hub\spleeter\{data.ytId}_vocals.wav'
+      with os.rename(old_name1,new_name1):
          print('Amended name of the vocal file')
 
-      old_name3 = r'C:\Users\Alva\Karaoke_Proj\BAD_Project_Gp1\media_hub\spleeter\accompaniment.wav'
-      new_name4 = r'C:\Users\Alva\Karaoke_Proj\BAD_Project_Gp1\media_hub\spleeter\{data.data.videoDetails.videoId}_accompaniment.wav'
-      with os.rename(old_name3,new_name4):
+      old_name2 = r'..\media_hub\spleeter\accompaniment.wav'
+      new_name2 = r'..\media_hub\spleeter\{data.ytId}_accompaniment.wav'
+      with os.rename(old_name2,new_name2):
          print('Amended name of the accompaniment file')
 
       #open a folder in S3 (name: the video ID)
@@ -37,13 +37,13 @@ def test(request):
       device = "cpu"
 
       model = whisperx.load_model("medium.en")
-      result = model.transcribe("SomeoneLikeYou.mp4", fp16=False, language='English' )
+      result = model.transcribe("../media_hub/spleeter/{data.ytId}_vocals.wav", fp16=False, language='English' )
       model_a, metadata = whisperx.load_align_model(language_code="en", device=device)
-      result_aligned = whisperx.align(result["segments"], model_a, metadata, "SomeoneLikeYou.mp4", device)
+      result_aligned = whisperx.align(result["segments"], model_a, metadata, "../media_hub/spleeter/{data.ytId}_vocals.wav", device)
       aligned_segments = result_aligned["segments"]
       aligned_word_segments = result_aligned["word_segments"]
 
-    #generate sentence-level srt
+      #generate sentence-level srt
       def generate_sentence_srt():
          count = 1
          for segment in aligned_segments:
@@ -55,7 +55,7 @@ def test(request):
             segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] is ' ' else text}\n\n"
 
 
-            srtFilename = os.path.join("../media_hub/SrtFiles", f"{data.data.videoDetails.videoId}_sentence.srt")
+            srtFilename = os.path.join("../media_hub/SrtFiles", f"{data.ytId}_sentence.srt")
             with open(srtFilename, 'a', encoding='utf-8') as srtFile:
                srtFile.write(segment)
 
@@ -73,7 +73,7 @@ def test(request):
             count+=1
             segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] is ' ' else text}\n\n"
 
-            srtFilename = os.path.join("../media_hub/SrtFiles", f"{data.data.videoDetails.videoId}_word.srt")
+            srtFilename = os.path.join("../media_hub/SrtFiles", f"{data.ytId}_word.srt")
             with open(srtFilename, 'a', encoding='utf-8') as srtFile:
                srtFile.write(segment)
 
@@ -90,13 +90,14 @@ def test(request):
 
 
       #MERGE THE AUDIO WITH VIDEO
-      input_video = ffmpeg.input("../media_hub/video/{data.videoDetails.videoId}.mp4")
-      input_audio = ffmpeg.input("../media_hub/spleeter/{data.videoDetails.videoId}_accompaniment.wav")
-      ffmpeg.concat(input_video, input_audio, v=1, a=1).output("../media_hub/combined/{data.videoDetails.videoId}_finished.mp4")
+      input_video = ffmpeg.input("../media_hub/video/{ytId}.mp4")
+      input_audio = ffmpeg.input("../media_hub/spleeter/{ytId}_accompaniment.wav")
+      with ffmpeg.concat(input_video, input_audio, v=1, a=1).output("../media_hub/combined/{ytId}_finished.mp4"):
+         print("Merged the audio with video")
 
       #MERGE THE VIDEO WITH SRT
-      srtFile = "../media_hub/SrtFile/{data.data.videoDetails.videoId}.srt"
-      input_video1 = "../media_hub/combined/{data.data.videoDetails.videoId}.mp4"
+      srtFile = "../media_hub/SrtFile/{data.ytId}.srt"
+      input_video1 = "../media_hub/combined/{data.ytId}.mp4"
 
 
       #save the finished video to S3
