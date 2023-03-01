c90b0018ed20bf8d147c22849ceacfa1d0c6c479
Alva Suen <ymsym98@gmail.com>
2023-03-01 18:35:46 +0800

    modified ytdlController and app.py

--- restful/controller/ytdlController.ts
+++ restful/controller/ytdlController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import ytdl from "ytdl-core";
 import { YtdlService } from "../service/ytdlService";
-import { createWriteStream } from "fs";
+import { createWriteStream} from "fs";
+import fs from "fs";
 import { errorHandler } from "../../error";
 import fetch from "cross-fetch";
+
 
 export class YtdlController {
   constructor(private ytdlService: YtdlService) {
@@ -17,13 +19,11 @@ export class YtdlController {
       console.log(req.body)
       ytdl.getInfo(URL as string).then(async (data) => {
         console.log(data);
-
-        // console.log(data.videoDetails.thumbnails.at(-1));
         
         //download audio only
         ytdl(URL as string, {
           filter: "audioonly",
-          quality: "highestaudio",
+          quality: "highestaudio"
         }).pipe(
           createWriteStream(
             `./media_hub/audio/${data.videoDetails.videoId}.mp3`
@@ -32,7 +32,7 @@ export class YtdlController {
 
         //download video only
         ytdl(URL as string, {
-          filter: "videoonly",
+          filter: "audioandvideo",
           quality: "highestvideo",
         }).pipe(
           createWriteStream(
@@ -43,20 +43,30 @@ export class YtdlController {
         await this.ytdlService.newSong(data.videoDetails.title, data.videoDetails.videoId, data.videoDetails.thumbnails.at(-1))
         
         // pass video data to sanic server
-        fetch("http://127.0.0.1:8080/sanicytdl", {
-          method: "POST",
-          headers: {
-            "Content-Type":"application/json"
-          },
-          body:JSON.stringify({
-            thumbnail: data.videoDetails.thumbnails.at(-1),
-            ytId: data.videoDetails.videoId,
-            name: data.videoDetails.title,
-            language: language,
-          })
-        }).then(()=>{
-          res.status(200).json({success:true});
-        })
+        // function checkFileExist (){
+        //   if (!fs.existsSync(`./media_hub/audio/${data.videoDetails.videoId}.mp3`) || !fs.existsSync(`./media_hub/video/${data.videoDetails.videoId}.mp4`)){
+        //     setTimeout(checkFileExist, 10000);
+        //   }else{
+        //     fetch("http://127.0.0.1:8080/sanicytdl", {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type":"application/json"
+        //   },
+        //   body:JSON.stringify({
+        //     thumbnail: data.videoDetails.thumbnails.at(-1),
+        //     ytId: data.videoDetails.videoId,
+        //     name: data.videoDetails.title,
+        //     language: language,
+        //   })
+        // }).then(()=>{
+        //   res.status(200).json({success:true});
+        // })
+        //   }
+        // }
+        
+        // checkFileExist ()
+
+        
       });
     } catch (err) {
        console.log(err);
--- sanic/app.py
+++ sanic/app.py
@@ -10,10 +10,12 @@ import ffmpeg
 
 app = Sanic("Karaoke")
 
-model_en = whisperx.load_model("medium.en")
-model_zh = whisperx.load_model("medium.zh")
+# model_en = whisperx.load_model("medium.en")
+# model_zh = whisperx.load_model("large")
 
-models = [model_en, model_zh]
+# models = [model_en, model_zh]
+
+model = whisperx.load_model("medium")
 
 
 def generate_sentence_srt(data, aligned_segments):
@@ -29,7 +31,7 @@ def generate_sentence_srt(data, aligned_segments):
         segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] is ' ' else text}\n\n"
 
         srtFilename = os.path.join(
-            "../media_hub/SrtFiles", f"{data.ytId}_sentence.srt")
+            "../media_hub/SrtFiles", f"{ytId}_sentence.srt")
         with open(srtFilename, 'a', encoding='utf-8') as srtFile:
             srtFile.write(segment)
 
@@ -50,7 +52,7 @@ def generate_word_srt(data, aligned_word_segments):
         segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] is ' ' else text}\n\n"
 
         srtFilename = os.path.join(
-            "../media_hub/SrtFiles", f"{data.ytId}_word.srt")
+            "../media_hub/SrtFiles", f"{ytId}_word.srt")
         with open(srtFilename, 'a', encoding='utf-8') as srtFile:
             srtFile.write(segment)
 
@@ -59,21 +61,23 @@ def generate_word_srt(data, aligned_word_segments):
 
 @app.post("/sanicytdl")
 def test(request):
+
     try:
         data = request.json
-        # print(data.ytId)
+        ytId = data["ytId"]
+        # print(ytId)
 
         #Step1: vocal and accompaniment separation
-        with subprocess.Popen(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', '../media_hub/spleeter', '{data.ytId}.mp3'], stdout=subprocess.PIPE) as proc:
+        with subprocess.Popen(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', '../media_hub/spleeter', '../media_hub/audio/{}.mp3'.format(ytId)], stdout=subprocess.PIPE) as proc:
             print(proc.stdout.read())
 
         old_name1 = r'..\media_hub\spleeter\vocals.wav'
-        new_name1 = r'..\media_hub\spleeter\{data.ytId}_vocals.wav'
+        new_name1 = r'..\media_hub\spleeter\{}_vocals.wav'.format(ytId)
         os.rename(old_name1, new_name1)
         print('Amended name of the vocal file')
 
         old_name2 = r'..\media_hub\spleeter\accompaniment.wav'
-        new_name2 = r'..\media_hub\spleeter\{data.ytId}_accompaniment.wav'
+        new_name2 = r'..\media_hub\spleeter\{}_accompaniment.wav'.format(ytId)
         os.rename(old_name2, new_name2)
         print('Amended name of the accompaniment file')
 
@@ -87,18 +91,18 @@ def test(request):
         if (data.language == "English"):
             video_language = "English"
             video_language_code = "en"
-            model=models[0]
+            # model=models[0]
         elif (data.languae == "mandarin"):
             video_language = "Chinese"
             video_language_code = "zh"
-            model=models[1]
+            # model=models[1]
 
         result = model.transcribe(
-            "../media_hub/spleeter/{data.ytId}_vocals.wav", fp16=False, language=video_language)
+            "../media_hub/spleeter/{}_vocals.wav".format(ytId), fp16=False, language=video_language)
         model_a, metadata = whisperx.load_align_model(
             language_code=video_language_code, device=device)
         result_aligned = whisperx.align(
-            result["segments"], model_a, metadata, "../media_hub/spleeter/{data.ytId}_vocals.wav", device)
+            result["segments"], model_a, metadata, "../media_hub/spleeter/{}_vocals.wav".format(ytId), device)
         aligned_segments = result_aligned["segments"]
         aligned_word_segments = result_aligned["word_segments"]
 
@@ -114,15 +118,15 @@ def test(request):
         # TO-BE-DONE
 
         # MERGE THE AUDIO WITH VIDEO
-        input_video = ffmpeg.input("../media_hub/video/{data.ytId}.mp4")
+        input_video = ffmpeg.input("../media_hub/video/{}.mp4".format(ytId))
         input_audio = ffmpeg.input(
-            "../media_hub/spleeter/{data.ytId}_accompaniment.wav")
-        with ffmpeg.concat(input_video, input_audio, v=1, a=1).output("../media_hub/combined/{data.ytId}_finished.mp4"):
+            "../media_hub/spleeter/{}_accompaniment.wav".format(ytId))
+        with ffmpeg.concat(input_video, input_audio, v=1, a=1).output("../media_hub/combined/{}_finished.mp4".format(ytId)):
             print("Merged the audio with video")
 
         # MERGE THE VIDEO WITH SRT
-        srtFile = "../media_hub/SrtFile/{data.ytId}.srt"
-        input_video1 = "../media_hub/combined/{data.ytId}.mp4"
+        srtFile = "../media_hub/SrtFile/{}.srt".format(ytId)
+        input_video1 = "../media_hub/combined/{}.mp4".format(ytId)
         # TO-BE-DONE
 
         # save the finished video to S3
