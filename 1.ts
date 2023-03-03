dce7dc3b7cfd5537dffc7fcfad4674f37c05092c
Gay <paul6541yau@gmail.com>
2023-03-03 17:08:15 +0800

    Created 1.ts

---
+++ 1.ts
@@ -0,0 +1,32 @@
+import { S3Client, GetObjectCommand, PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+// Constants:
+const MRAP = "karaoke-gcat";
+// const BODY = "hello-world";
+const KEY = "k4V3Mo61fJM.mp4"
+
+const client = new S3Client({ region: "ap-southeast-1", disableMultiregionAccessPoints: true });
+
+// async function put() {
+//     try {
+//         await client.send(new PutObjectCommand({
+//             Bucket: MRAP,
+//             Key: KEY,
+//             // Body: BODY
+//         }))
+//     } catch (error) {
+//         console.log('put function failed')
+//     }
+// }
+// put() // put on mrap works
+
+async function signGet() {
+    const getCommand = new GetObjectCommand({
+        Bucket: MRAP,
+        Key: KEY
+    })
+    const signedUrl = await getSignedUrl(client, getCommand, { expiresIn: 3600 });
+    console.log(signedUrl)
+}
+signGet()
