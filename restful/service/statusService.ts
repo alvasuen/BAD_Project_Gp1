66805623f577dc9e791b4bf5adc4769dfd8e427c
Alva Suen <ymsym98@gmail.com>
2023-03-04 15:13:48 +0800

    add login guard to status route

--- restful/service/statusService.ts
+++ restful/service/statusService.ts
@@ -1,20 +1,25 @@
 import {Knex} from "knex";
 
 export class StatusService {
+
     constructor(private knex: Knex){
         this.knex =knex;
     }
-    statusCheck= async (id: number)=>{
-        let check = await this.knex.select ("status_id", "created_at").from("download_status").where("message", "In Progress")
-        for (let i =0; i< check.length; i++){
-            const currDate = new Date ()
-            const oldDate = new Date (check[i].created_at)
-            let diff = (currDate.getTime()-oldDate.getTime())/60000
-            if (diff>120){
-                await this.knex("download_status").update("message","Download timeout!").where("status_id", check[i].status_id)
+
+        statusCheck= async (id: number)=>{
+            
+            let check = await this.knex.select ("status_id", "created_at").from("download_status").where("message", "In Progress")
+            for (let i =0; i< check.length; i++){
+                const currDate = new Date ()
+                const oldDate = new Date (check[i].created_at)
+                let diff = (currDate.getTime()-oldDate.getTime())/60000
+                console.log({diff});
+                if (diff>120){
+                    await this.knex("download_status").update("message","Download timeout!").where("status_id", check[i].status_id)
+                }
             }
+            let result = await this.knex.select("title", "image", "status", "message").from ("download_status").where ("users_id", id).orderBy("status_id", "DESC")
+            return result;
         }
-        let result = await this.knex.select("title", "image", "status", "message").from ("download_status").where ("users_id", id).orderBy("status_id", "DESC")
-        return result;
-    }
+
 }
