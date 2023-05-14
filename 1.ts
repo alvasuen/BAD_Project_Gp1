65b83facbd8642d8234efcaa39ddd5f215716a22
xxxyauxxx <paul6541yau@gmail.com>
2023-05-15 01:46:31 +0800

    Changed let AWS_ACCESS_KEY = env.accessKey; in 1.ts, last is app.py in Sanic

--- 1.ts
+++ 1.ts
@@ -1,27 +1,33 @@
-import { S3Client, GetObjectCommand, PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  CreateBucketCommand,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { env } from "./env";
 
 // Constants:
 const MRAP = "karaoke-gcat";
 // const BODY = "hello-world";
-const KEY = "logo3.PNG"
-let AWS_ACCESS_KEY = "AKIAVFNJBF6YRDGBDJYL"
-let AWS_ACCESS_SECRET_KEY = "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"
+const KEY = "logo3.PNG";
+let AWS_ACCESS_KEY = env.accessKey;
+let AWS_ACCESS_SECRET_KEY = "F1qzQ+ZH9NzWf8+gMh4aPAah9lMvUBl5uqyGrlt9";
 const client = new S3Client({
-    region: "ap-southeast-1",
-    disableMultiregionAccessPoints: true,
-    credentials: {
-        accessKeyId: AWS_ACCESS_KEY,
-        secretAccessKey: AWS_ACCESS_SECRET_KEY
-    }
+  region: "ap-southeast-1",
+  disableMultiregionAccessPoints: true,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_ACCESS_SECRET_KEY,
+  },
 });
 
 async function signGet() {
-    const getCommand = new GetObjectCommand({
-        Bucket: MRAP,
-        Key: KEY,
-    })
-    const signedUrl = await getSignedUrl(client, getCommand, { expiresIn: 3600 });
-    console.log(signedUrl)
+  const getCommand = new GetObjectCommand({
+    Bucket: MRAP,
+    Key: KEY,
+  });
+  const signedUrl = await getSignedUrl(client, getCommand, { expiresIn: 3600 });
+  console.log(signedUrl);
 }
-signGet()
+signGet();
